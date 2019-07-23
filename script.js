a0add0bc5ccecc6409e76af86f18cfca6c95bdc9
nrbozzelli <nrbozzelli@gmail.com>
2019-07-23 14:27:52 -0600

    Added winning conditions to the game.

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
 // global variables
+const startButton = document.getElementById('start');
 const doorImage1 = document.getElementById('door1');
 const doorImage2 = document.getElementById('door2');
 const doorImage3 = document.getElementById('door3');
 const botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg'
 const beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
 const spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
-const numClosedDoors = 3;
+const closedDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg';
+let numClosedDoors = 3;
 let openDoor1;
 let openDoor2;
 let openDoor3;
-
-// onclick functions for each door
-doorImage1.onclick = () => {
-    doorImage1.src = openDoor1;
-}
-
-doorImage2.onclick = () => {
-    doorImage2.src = openDoor2;
-}
-
-doorImage3.onclick = () => {
-    doorImage3.src = openDoor3;
-}
 
 // random chore generator function
 const randomChoreDoorGenerator = () => {
@@ -42,3 +31,47 @@ const randomChoreDoorGenerator = () => {
 }
 
 randomChoreDoorGenerator();
+
+// gameplay functions
+const isClicked = (door) => {
+    if(door.src === closedDoorPath) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+const playDoor = () => {
+    numClosedDoors--;
+    if(numClosedDoors === 0) {
+        gameOver('win');
+    }
+}
+
+const gameOver = (status) => {
+    if(status === 'win') {
+        startButton.innerHTML = 'You win! Play again?';
+    }
+}
+
+// onclick functions for each door
+doorImage1.onclick = () => {
+    if(!isClicked(doorImage1)) {
+        doorImage1.src = openDoor1;
+        playDoor();
+    }
+}
+
+doorImage2.onclick = () => {
+    if(!isClicked(doorImage2)) {
+        doorImage2.src = openDoor2;
+        playDoor();
+    }
+}
+
+doorImage3.onclick = () => {
+    if(!isClicked(doorImage3)) {
+        doorImage3.src = openDoor3;
+        playDoor();
+    }
+}
