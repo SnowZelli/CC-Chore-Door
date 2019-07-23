baffdd57cc4b71e9f2a79b525398fe520c826acc
nrbozzelli <nrbozzelli@gmail.com>
2019-07-23 16:40:03 -0600

    Added some more logic to the game. Still working through a bug.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // global variables
-const startButton = document.getElementById('start');
-const doorImage1 = document.getElementById('door1');
-const doorImage2 = document.getElementById('door2');
-const doorImage3 = document.getElementById('door3');
-const botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg'
-const beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
-const spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
-const closedDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg';
+let startButton = document.getElementById('start');
+let door1 = document.getElementById('door1');
+let door2 = document.getElementById('door2');
+let door3 = document.getElementById('door3');
+let botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg'
+let beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
+let spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
+let closedDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg';
 let numClosedDoors = 3;
 let openDoor1;
 let openDoor2;
@@ -15,6 +15,8 @@ let openDoor3;
 // random chore generator function
 const randomChoreDoorGenerator = () => {
     let choreDoor = Math.floor(Math.random() * numClosedDoors);
+    //cheat to let me know what door the bot is behind.
+    console.log(choreDoor);
     if (choreDoor === 0) {
         openDoor1 = botDoorPath;
         openDoor2 = beachDoorPath;
@@ -23,16 +25,17 @@ const randomChoreDoorGenerator = () => {
         openDoor1 = spaceDoorPath;
         openDoor2 = botDoorPath;
         openDoor3 = beachDoorPath;
-    } else if(choreDoor ===2) {
+    } else if(choreDoor === 2) {
         openDoor1 = beachDoorPath;
         openDoor2 = spaceDoorPath;
         openDoor3 = botDoorPath;
     }
 }
 
+// function calls
 randomChoreDoorGenerator();
 
-// gameplay functions
+// gameply functions
 const isClicked = (door) => {
     if(door.src === closedDoorPath) {
         return false;
@@ -41,37 +44,50 @@ const isClicked = (door) => {
     }
 }
 
-const playDoor = () => {
-    numClosedDoors--;
-    if(numClosedDoors === 0) {
-        gameOver('win');
+const isBot = (door) => {
+    if(door === botDoorPath) {
+        return true;
+    } else {
+        return false;
     }
 }
 
 const gameOver = (status) => {
     if(status === 'win') {
         startButton.innerHTML = 'You win! Play again?';
+    } else if (status === 'lose') {
+        startButton.innerHTML = 'Game Over! Play again?';
     }
 }
+
+const playDoor = (door) => {
+    numClosedDoors--;
+    if(numClosedDoors === 0) {
+        gameOver('win');
+    } else if (isBot(door)) {
+        gameOver('lose');
+    }
+}
+
 
 // onclick functions for each door
-doorImage1.onclick = () => {
-    if(!isClicked(doorImage1)) {
-        doorImage1.src = openDoor1;
-        playDoor();
+door1.onclick = () => {
+    if(!isClicked(door1)) {
+        door1.src = openDoor1;
+        playDoor(door1);
     }
 }
 
-doorImage2.onclick = () => {
-    if(!isClicked(doorImage2)) {
-        doorImage2.src = openDoor2;
-        playDoor();
+door2.onclick = () => {
+    if(!isClicked(door2)) {
+        door2.src = openDoor2;
+        playDoor(door2);
     }
 }
 
-doorImage3.onclick = () => {
-    if(!isClicked(doorImage3)) {
-        doorImage3.src = openDoor3;
-        playDoor();
+door3.onclick = () => {
+    if(!isClicked(door3)) {
+        door3.src = openDoor3;
+        playDoor(door3);
     }
 }
